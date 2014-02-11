bf6171b6decc0aff82441903e566683455bab86b
lotaku <317399510@qq.com>
2014-02-11 12:56:30 +0800

    鼠标划过时女孩出现

--- board-game.py
+++ board-game.py
@@ -32,7 +32,7 @@ BOXCOLOR = WHITE
 HIGHLIGHTCOLOR = BLUE
 
 def main():
-    global FPSCLOCK, DISPLAYSURF
+    global FPSCLOCK, DISPLAYSURF,catImg,boyImg
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF=pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
@@ -41,12 +41,19 @@ def main():
     mousey = 0
     pygame.display.set_caption('Board Game')
 
+
+
+    #mainBoard = getRandomizedBoard()
+    revealedBoxes = generateRevealedBoxesData(False)
+
     while True:
         mouseClicked = False
 
         DISPLAYSURF.fill(BGCOLOR)
         drawBoard()
 
+        catImg = pygame.image.load('catgirl.png')
+        boyImg = pygame.image.load('boy.png')
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 pygame.quit()
@@ -56,15 +63,44 @@ def main():
             elif event.type == MOUSEBUTTONUP:
                 mousex, mousey = event.pos
                 mouseClicked = True
-
+        boxx, boxy = getBoxAtPixel(mousex, mousey)
+        if boxx != None and boxy != None:
+            # The mouse is currently over a box.
+            if not revealedBoxes[boxx][boxy]:
+                drawHighlightBox(boxx, boxy)
+            if not revealedBoxes[boxx][boxy] and mouseClicked:
+                #revealBoxesAnimation(mainBoard, [(boxx, boxy)])
+                drawGirl(boxx, boxy)
+                revealedBoxes[boxx][boxy] = True # set the box as "revealed"
         pygame.display.update()
         FPSCLOCK.tick(FPS)
+
+def drawGirl(boxx, boxy):
+    left, top = leftTopCoordsOfBox(boxx, boxy)
+    DISPLAYSURF.blit(boyImg, (left,top))
+
+def drawHighlightBox(boxx, boxy):
+    left, top = leftTopCoordsOfBox(boxx, boxy)
+    #pygame.draw.rect(DISPLAYSURF, HIGHLIGHTCOLOR, (left - 5, top - 5, BOXSIZE + 10, BOXSIZE + 10), 4)
+    DISPLAYSURF.blit(catImg, (left,top))
 
 def leftTopCoordsOfBox(boxx, boxy):
     # Convert board coordinates to pixel coordinates
     left = boxx * (BOXSIZE + GAPSIZE) + XMARGIN
     top = boxy * (BOXSIZE + GAPSIZE) + YMARGIN
     return (left, top)
+
+
+
+def getBoxAtPixel(x, y):
+    for boxx in range(BOARDWIDTH):
+        for boxy in range(BOARDHEIGHT):
+            left, top = leftTopCoordsOfBox(boxx, boxy)
+            boxRect = pygame.Rect(left, top, BOXSIZE, BOXSIZE)
+            if boxRect.collidepoint(x, y):
+                return (boxx, boxy)
+    return (None, None)
+
 
 def drawBoard():
     # Draws all of the boxes at the beginning of game.
@@ -73,6 +109,18 @@ def drawBoard():
             left, top = leftTopCoordsOfBox(boxx, boxy)
             pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, BOXSIZE, BOXSIZE))
 
+#def drawBoard(board, revealed):
+    ## Draws all of the boxes in their covered or revealed state.
+    #for boxx in range(BOARDWIDTH):
+        #for boxy in range(BOARDHEIGHT):
+            #left, top = leftTopCoordsOfBox(boxx, boxy)
+            #if not revealed[boxx][boxy]:
+                ## Draw a covered box.
+                #pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, BOXSIZE, BOXSIZE))
+            #else:
+                ## Draw the (revealed) icon.
+                #shape, color = getShapeAndColor(board, boxx, boxy)
+                #drawIcon(shape, color, boxx, boxy)
 def getRandomizedBoard():
     # Get a list of every possible shape in every possible color.
     icons = []
@@ -94,6 +142,14 @@ def getRandomizedBoard():
             del icons[0] # remove the icons as we assign them
         board.append(column)
     return board
+
+
+def generateRevealedBoxesData(val):
+    revealedBoxes = []
+    for i in range(BOARDWIDTH):
+        revealedBoxes.append([val] * BOARDHEIGHT)
+    return revealedBoxes
+
 
 
 if __name__ == '__main__':
