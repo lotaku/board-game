d8e905ee24795d834bfd49fe7ae88cdaefb97610
lotaku <317399510@qq.com>
2014-03-01 16:28:25 +0800

    清除多余的注释代码

--- server/player.py
+++ server/player.py
@@ -9,6 +9,7 @@ class Player:
         self.sendData  = ''
         self.broadBuff = ''
         self.exitKey = 0
+        self.team=""
     def creat(self,name):
         self.name=name
         self.x=0
@@ -28,7 +29,6 @@ class Player:
         self.x=newX
         self.y=newY
         self.gs2cPlayerMove()
-        #self.gs2cExistingPlayers()
         self.gs2cOtherMove()
     def gs2cPlayerMove(self):
         packet=SendPacket(2)
@@ -37,11 +37,6 @@ class Player:
         packet.packString(self.name)
         packet.send(self)
     def gs2cOhterEnterWorld(self):
-        #packet=SendPacket(3)
-        #packet.packInt(self.x)
-        #packet.packInt(self.y)
-        #packet.packString(self.name)
-        #packet.sendOther(self)
         for _,playerOther in playerManager.socketPlayer.items():
             packet=SendPacket(3)
             packet.packInt(self.x)
@@ -55,11 +50,6 @@ class Player:
             packet.packInt(self.y)
             packet.packString(self.name)
             packet.send(playerOther)
-        #packet=SendPacket(4)
-        #packet.packInt(self.x)
-        #packet.packInt(self.y)
-        #packet.packString(self.name)
-        #packet.sendOther(self)
 
     def gs2cExistingPlayers(self):
         """S将其他玩家的x，y ，name 发给 刚刚加入的 player"""
@@ -75,29 +65,16 @@ class Player:
             packet=SendPacket(0)
             packet.packString(self.name)
             packet.send(playerOther)
-
-        #self.exitKey=1
-        #packet=SendPacket(0)
-        #packet.packString(self.name)
-        #packet.sendOther(self)
 def c2gsEnterWorld(player,packet):
     name = packet.unpackString()
     player.creat(name)
     player.enterWorld()
-    #player.gs2cOtherMove()
 def c2gsPlayerMove(player,packet):
     x=packet.unpackInt()
     y=packet.unpackInt()
-    #name=packet.unpackString()
-    #Cplayer = playerManager
-    #player = playerManager.get(player.socket)
-    #player.name = name
     player.move(x,y)
 
 def c2gsExitGame(player,packet):
-    #x=packet.unpackInt()
-    #y=packet.unpackInt()
-    #name = packet.unpackString()
     player.gs2cOtherExitGame()
 
 
