d71e25f12bbdef1ac96472e9bbecb873320d168f
lotaku <317399510@qq.com>
2014-03-01 22:24:51 +0800

    添加id6:c2gsTeamCreate()

--- server/opccode.py
+++ server/opccode.py
@@ -2,9 +2,10 @@
 # encoding: utf-8
 import player
 handler={
+    0:player.c2gsExitGame,
     1:player.c2gsEnterWorld,
     2:player.c2gsPlayerMove,
-    0:player.c2gsExitGame,
+    6:player.c2gsTeamCreate
         }
 def handlePacket(player,packet):
     handler[packet.id](player,packet)
