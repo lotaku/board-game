eed555a61f3f9785cea843f279ea3255f24378e7
lotaku <317399510@qq.com>
2014-03-01 22:30:02 +0800

    初步通过>右击菜单>创建队伍(单客户端)

--- client/gwdata.py
+++ client/gwdata.py
@@ -7,7 +7,8 @@ import player
 from pygame.locals import *
 from player_manager import playerManager
 import time
-
+from team import team
+#from teamManager import teamManager
 
 
 EXITKEY=0
@@ -27,7 +28,7 @@ YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
 BOXX_MOVE_TEMP = 999
 BOXY_MOVE_TEMP = 999
 
-
+MENUCURRENT ={}
 girlImg = pygame.image.load("../catgirl.png")
 boyImg = pygame.image.load('../boy.png')
 
@@ -47,10 +48,12 @@ BGCOLOR = NAVYBLUE
 LIGHTBGCOLOR = GRAY
 BOXCOLOR = WHITE
 HIGHLIGHTCOLOR = BLUE
+HAVE_DRAW_TEAM_MEMBER =0
 def loginWin():
-    global DISPLAYSURF, player
+    global DISPLAYSURF, player,FONT_OBJ#team ,teamManager
     pygame.init()
     #FPSCLOCK=pygame.time.Clock()
+    FONT_OBJ = pygame.font.Font('freesansbold.ttf', 22)
     DISPLAYSURF=pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
 
     pygame.display.set_caption("Board Game")
@@ -155,12 +158,9 @@ def getBoxAtPixel(x, y):
     return (None, None)
 
 def playermove():
-    global EXITKEY
-    #DISPLAYSURF.fill(BGCOLOR)
-    #mouseClicked = False
+    global EXITKEY,MENUCURRENT
     mousex = 0
     mousey = 0
-    #print "play函数，获取event"
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
             player.player.c2gsExitGame()
@@ -176,19 +176,94 @@ def playermove():
                     #if boxx != x or boxy != y:
                         ##print "调用drawPlaye"
                         #drawPlayerTemp(player.player,boxx,boxy)
-
-        elif event.type == MOUSEBUTTONUP:
-            mousex,mousey = event.pos
-            boxx,boxy = getBoxAtPixel(mousex,mousey)
-            #确定移动前，临时显示player
-            if boxx !=None and boxy != None:
-                player.player.c2gsPlayerMove(boxx,boxy)
-        #elif exitKey==1:
-            #pygame.quit()
-            #sys.exit()
+        elif event.type == MOUSEBUTTONDOWN and not len(MENUCURRENT):
+            if event.button == 1:
+                mousex,mousey = event.pos
+                boxx,boxy = getBoxAtPixel(mousex,mousey)
+                if boxx !=None and boxy != None:
+                    player.player.c2gsPlayerMove(boxx,boxy)
+            if event.button == 3:  # 鼠标右击
+                mousex,mousey = event.pos
+                boxx,boxy = getBoxAtPixel(mousex,mousey)
+                if boxx != None and boxy != None:
+                    #todo:if 这个位置有玩家
+                    global LastBoxx,LastBoxy
+                    MENUCURRENT, LastBoxx,LastBoxy= drawCurrentMenu(boxx,boxy)
+        elif event.type == MOUSEBUTTONDOWN and len(MENUCURRENT):
+            if event.button == 1:
+                mousex,mousey = event.pos
+                mouseLClickOnCurrentMenu(MENUCURRENT, LastBoxx,LastBoxy,mousex,mousey)
+    #drawTeamMember() # 又一次设置了全局函数
+                #测试右键效果
+                #testFont,testRect = makeText("xxx",RED,BGCOLOR,10,10)
+                #DISPLAYSURF.blit(testFont, testRect)
 
 def exitGame():
     if EXITKEY:
         #time.sleep(3)
         pygame.QUIT
         sys.exit()
+def mouseRClick():
+    for event in pygame.event.get():
+        if event.type == MOUSEBUTTONDOWN:
+            if event.button == 3:  # 鼠标右击
+                #testFont= FONT_OBJ.render('xxx', True, NAVYBLUE)
+                testFont = makeText("xxx",10,10)
+                DISPLAYSURF.blit(testFont, (10,10))
+def makeText(text,color,bgcolor,top,left):
+    """返回 textSurf,textRect"""
+    FONT_OBJ= pygame.font.Font('freesansbold.ttf', 22)
+    textSurf=FONT_OBJ.render(text,True,color,bgcolor)
+    textRect=textSurf.get_rect()
+    textRect.topleft=(top,left)
+    return (textSurf,textRect)
+
+def drawCurrentMenu(boxx,boxy):
+    LastBoxx,LastBoxy = boxx,boxy
+    left,top = leftTopCoordsOfBox(boxx+1, boxy)
+    width =BOXSIZE
+    height=BOXSIZE*2+GAPSIZE
+    menuLineRect = pygame.draw.rect(DISPLAYSURF,RED,(left,top,width,height))
+    #获得当前菜单:
+    #menuCurrent={0:createTeam(),1:'def2'}
+    #menuCurrent={0:("TeamCreat",player.player.c2gsTeamCreate()),1:('menu2','def2')}
+    menuCurrent={0:("TeamCreat",player.player.c2gsTeamCreate),1:('menu2','def2')}
+    for i in range(len(menuCurrent)):
+        print '画菜单'
+        menuLineHeight = BOXSIZE/2
+        menuLineSurf,menuLineRect = makeText(menuCurrent[i][0],WHITE,BGCOLOR,left,top+menuLineHeight*i)
+        DISPLAYSURF.blit(menuLineSurf,menuLineRect)
+    return menuCurrent,LastBoxx,LastBoxy
+def mouseLClickOnCurrentMenu(menuCurrent, LastBoxx,LastBoxy,mousex,mousey):
+    left,top = leftTopCoordsOfBox(LastBoxx+1, LastBoxy)
+    width =BOXSIZE
+    height=BOXSIZE*2+GAPSIZE
+    menuRect = pygame.Rect(left,top,width,height)
+    if menuRect.collidepoint(mousex,mousey):
+        for i in range(len(menuCurrent)):
+            menuLineHeight = BOXSIZE/2
+            _,menuLineRect = makeText(menuCurrent[i][0],WHITE,BGCOLOR,left,top+menuLineHeight*i)
+            if menuLineRect.collidepoint(mousex,mousey):
+                #testDef(menuCurrent[i][0])
+                menuCurrent[i][1]()
+                #print 'xxx',player.team.member
+                #pygame.time.wait(3000)
+def drawTeamMember():
+    #global HAVE_DRAW_TEAM_MEMBER
+    #if not HAVE_DRAW_TEAM_MEMBER:
+    print '成员数,',len(player.team.member)
+    for memberName in player.team.member:
+        for i in range(len(player.team.member)):
+            memberSurf,memberRect = makeText(memberName,RED,GREEN,20+(66*i),20)
+            DISPLAYSURF.blit(memberSurf,memberRect)
+        #HAVE_DRAW_TEAM_MEMBER = 1
+
+
+def testDef(text):
+    testFont,testRect = makeText(text,RED,BGCOLOR,10,10)
+    DISPLAYSURF.blit(testFont, testRect)
+
+
+
+
+
