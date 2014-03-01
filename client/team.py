513a3113c32ddd0f50f70f78e1d4893b737fb19c
lotaku <317399510@qq.com>
2014-03-01 22:09:15 +0800

    创建client的team,和teammanagre类

---
+++ client/team.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+class Team():
+    def __init__(self):
+        self.caption = ''
+        #self.name=""
+        self.member=[]
+    def create(self,player):
+        self.caption = player.name
+        self.name="TN"+player.name
+        self.member.append(player.name)
+        player.team=self
+    def add(self,player):
+        self.member.append(player)
+    def remove(self,player):
+        self.member.remove(player)
+team=Team()
+
